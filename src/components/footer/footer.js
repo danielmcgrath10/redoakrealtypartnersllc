09586121ae36e2682d45dad8a292b9968ea11a60
Daniel McGrath <danmcgrath1035@gmail.com>
2020-09-05 14:10:48 -0400

    good form

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,47 +4,85 @@ import FB from '../../img/facebook.svg';
 import Insta from '../../img/instagram.svg';
 
 export default function Footer(props){
+
+    const submitForm = (e) => {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                event.preventDefault();
+                event.stopPropagation();
+                form.classList.add('was-validated');
+
+                console.log(form.inputFirstName.value);
+            }, false);
+        });
+    }
+
     return(
         <div id={'footer'}>
             <div id={'left'}>
-                <form id={'interest-form'}>
+                <form id={'interest-form'} className={'needs-validation'}>
                     <span id={'form-title'} className={'footer-text'}>Get In Touch</span>
                     <div className={'form-row'}>
                         <div className="form-group col-md-6">
-                            <label for="inputFirstName">First Name</label>
-                            <input type="text" className="form-control" id="inputFirstName" placeholder={'John'}/>
+                            <label htmlFor="inputFirstName">First Name</label>
+                            <input type="text" className="form-control" id="inputFirstName" placeholder={'John'} required/>
+                            <div className="invalid-feedback">
+                                Please provide a valid First Name.
+                            </div>
                         </div>
                         <div className="form-group col-md-6">
-                            <label for="inputLastName">Last Name</label>
-                            <input type="text" className="form-control" id="inputLastName" placeholder={'Smith'}/>
+                            <label htmlFor="inputLastName">Last Name</label>
+                            <input type="text" className="form-control" id="inputLastName" placeholder={'Smith'} required/>
+                            <div className="invalid-feedback">
+                                Please provide a valid Last Name.
+                            </div>
                         </div>
                     </div>
                     <div className="form-group">
-                        <label for="inputEmail">Email</label>
-                        <input type="email" className="form-control" id="inputEmail" placeholder="xxx@xxx.xxx"/>
+                        <label htmlFor="inputEmail">Email</label>
+                        <input type="email" className="form-control" id="inputEmail" placeholder="xxx@xxx.xxx" required/>
+                        <div className="invalid-feedback">
+                            Please provide a valid Email Address.
+                        </div>
                     </div>
                     <div className="form-group">
-                        <label for="inputAddress">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                        <label htmlFor="inputAddress">Address</label>
+                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" required/>
+                        <div className="invalid-feedback">
+                            Please provide an Address.
+                        </div>
                     </div>
                     <div className="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="inputCity">City</label>
-                            <input type="text" className="form-control" id="inputCity"/>
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputCity">City</label>
+                            <input type="text" className="form-control" id="inputCity" required/>
+                            <div className="invalid-feedback">
+                                Please provide a valid City.
+                            </div>
                         </div>
                         <div className="form-group col-md-4">
-                            <label for="inputState">State</label>
-                            <select id="inputState" className="form-control">
+                            <label htmlFor="inputState">State</label>
+                            <select id="inputState" className="form-control" required>
                                 <option selected>Choose...</option>
                                 <option>...</option>
                             </select>
+                            <div className="invalid-feedback">
+                                Please provide a City.
+                            </div>
                         </div>
                             <div className="form-group col-md-2">
-                            <label for="inputZip">Zip</label>
+                            <label htmlFor="inputZip">Zip</label>
                             <input type="text" className="form-control" id="inputZip"/>
                         </div>
                     </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button onClick={submitForm} type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
             <div id={'right'}>
